cbdb00245d977f22d112f90f815c5497048de739
ScottShadow <rudaseswascottmc@gmail.com>
2024-07-03 16:40:23 +0200

    minor fix

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
+"""
+    A function that concatenates two strings and returns the result.
+"""
+
+
 def concat(str1: str, str2: str) -> str:
+    """
+    A function that concatenates two strings and returns the result.
+
+    Args:
+        str1 (str): The first string to concatenate.
+        str2 (str): The second string to concatenate.
+
+    Returns:
+        str: The concatenated string of str1 and str2.
+    """
     return str1+str2
