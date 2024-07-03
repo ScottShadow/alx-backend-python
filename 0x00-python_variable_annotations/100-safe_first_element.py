773e07c73fbe07cd124b3f04be56f418d51ba7c3
ScottShadow <rudaseswascottmc@gmail.com>
2024-07-03 21:43:03 +0200

    added files

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Blind Typing
+"""
+# The types of the elements of the input are not know
+from typing import Any, List, Optional
+
+
+def safe_first_element(lst: List[Any]) -> Optional[Any]:
+    """
+    Returns the first element of the list if it is not empty, otherwise returns None.
+
+    Args:
+        lst (List[Any]): The input list.
+
+    Returns:
+        Optional[Any]: The first element of the list if it is not empty, otherwise None.
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
