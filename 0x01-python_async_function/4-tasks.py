ff4c3f00dc94e4aeea87f899ea214a3655b5adac
ScottShadow <rudaseswascottmc@gmail.com>
2024-07-08 21:08:09 +0200

    fixes

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
+"""
+This module contains a function that creates a list of
+ tasks for a coroutine
+"""
 import asyncio
-
-"""
-This module contains a function that creates a list of tasks for a coroutine
-"""
 from typing import List
 task_wait_random = __import__("3-tasks").task_wait_random
 
@@ -15,8 +15,9 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
         n (int): The number of times to call wait_random
         max_delay (int): The maximum delay for each coroutine
     Returns:
-        List[float]: The list of delays
+        List[float]: The sorted list of delays
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     time_array = await asyncio.gather(*tasks)
+    time_array.sort()
     return time_array
