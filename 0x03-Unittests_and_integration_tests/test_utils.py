305427a6c3485d24a34101b1ebae75ce769067e0
ScottShadow <rudaseswascottmc@gmail.com>
2024-07-26 15:29:11 +0200

    added memoize tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,11 +5,13 @@ Parameterize Unit testing
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """TESTING UTILS"""
+    """
+    Test the `access_nested_map` function with different input parameters.
+    """
     @parameterized.expand([
         ({"a": 1}, ("a"), (1)),
         ({"a": {"b": 2}}, ("a"), ({"b": 2})),
@@ -62,7 +64,9 @@ class TestAccessNestedMap(unittest.TestCase):
 
 
 class TestGetJson(unittest.TestCase):
-    """TESTING UTILS"""
+    """
+    Test the `get_json` function with different input parameters.
+    """
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
@@ -99,6 +103,64 @@ class TestGetJson(unittest.TestCase):
 
         # Verify that the parsed payload matches the expected payload
         self.assertEqual(res, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+        Test the `memoize` decorator by verifying that it correctly
+    """
+
+    def test_memoize(self):
+        """
+        Test the `memoize` decorator by verifying that it correctly
+        memoizes the result of a method.
+
+        This test case mocks the `a_method` of the `TestClass` and sets
+        its return value to 42. It then creates an instance of `TestClass`
+        and calls its `a_property` method twice. The `memoize` decorator is
+        used to memoize the result of `a_method`.
+
+        After calling `a_property` twice, the test case verifies that
+        `a_method` is only called once by asserting that
+        `mock_method.assert_called_once()` is `True`. It also verifies
+        that the memoized result of `a_property` is equal to 42 byasserting
+        that `self.assertEqual(mock_method, 42)` is `True`.
+
+        Parameters:
+            - self (TestCase): The test case instance.
+            - mock_method (MagicMock): The mock object for the `a_method`
+            method of the `TestClass`.
+
+        Returns:
+            None
+        """
+
+        # The class that contains the methods to be tested
+        class TestClass:
+            """Test class"""
+
+            # The method to be memoized
+            def a_method(self):
+                """a_method"""
+                return 42
+
+            # The method to be memoized
+            @memoize
+            def a_property(self):
+                """a_property: Property decorated with memoize"""
+                return self.a_method()
+
+        # Mock the method to be tested
+        with patch.object(TestClass, "a_method") as mock_method:
+            # Create an instance of the test class
+            myobject = TestClass()
+
+            # Call the property method twice
+            myobject.a_property()
+            myobject.a_property()
+
+            # Verify that the method was only called once
+            mock_method.assert_called_once()
 
 
 if __name__ == "__main__":
