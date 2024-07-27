df6828dd8df57a43748dcf5d314387f63ea1267a
ScottShadow <rudaseswascottmc@gmail.com>
2024-07-27 20:42:04 +0200

    added test for publick_repos

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -62,6 +62,51 @@ class TestGithubOrgClient(unittest.TestCase):
         # Assert that the org property was called once
         mock_org.assert_called_once()
 
+    @parameterized.expand([
+        # Test case where the org "google" is used
+        # and the payload is "GOOGLE(list)"
+        ("google", ["GOOGLE"],
+         [{"name": "GOOGLE"}]),
+        # Test case where the org "random_url" is used
+        # and the payload is "RANDO(list)"
+        ('random_url', ["RANDO"],
+         [{"name": "RANDO"}])
+    ])
+    @patch("client.get_json")
+    def test_public_repos(self, test_org, payload, json_payload, mock_get):
+        """
+        Test the public repos method of the GithubOrgClient class.
+
+        This test case checks if the public repos method of
+        GithubOrgClient class returns the correct value based on the
+        payload returned from the _public_repos_url property and
+        the get_json function.
+
+        Args:
+            test_org (str): The name of the organization.
+            payload (list): A list containing the expected payload.
+            json_payload (list): A list containing the JSON payload.
+            mock_get (MagicMock): A mock object for the get_json function.
+
+        Returns:
+            None
+        """
+        # Mock the _public_repos_url property
+        with patch("client.GithubOrgClient._public_repos_url",
+                   new_callable=PropertyMock) as mock_pr:
+            mock_pr.return_value = payload
+            # Mock the get_json function
+            mock_get.return_value = json_payload
+
+            # Call the public_repos method
+            res = GithubOrgClient(test_org).public_repos()
+            # Assert that the returned value is equal to the payload
+            self.assertEqual(res, payload)
+            # Assert that the _public_repos_url property was called once
+            mock_pr.assert_called_once()
+            # Assert that the get_json function was called once
+            mock_get.assert_called_once()
+
 
 if __name__ == "__main__":
     unittest.main()
